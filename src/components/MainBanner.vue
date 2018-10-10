0e2e323592b5c85dd221e52718784046598e02bd
Christiaan Quyn <christianq010@gmail.com>
2018-10-10 09:58:48 +0530

    Add main banner to home page view

---
+++ src/components/MainBanner.vue
@@ -0,0 +1,148 @@
+<template>
+    <!-- Main Section -->
+    <!-- Main Content Wrapper with image-->
+        <div class="main-content-wrapper" 
+            v-bind:style="{ 'background-image': 'url(' + fields.url + ')' }">
+            <!-- Overlay -->
+            <div class="main-content-overlay">
+                <!-- Main Heading -->
+                <div class="main-content-h1-w">
+                  <router-link to="/article/trump-shows-trade-true-colors" v-bind:fields="fields" tag="div">
+                    <div class="main-content-h1">
+                        {{ $prismic.richTextAsPlain(fields.title) }}
+                    </div>
+                  </router-link>
+                </div>
+                <!-- /end of Main heading -->
+            </div>
+            <!-- /end of Overlay -->
+        </div>
+        <!-- Main Content Wrapper -->
+    <!-- /end of Main Section -->
+</template>
+
+<script>
+export default {
+  name: 'Main Banner',
+  data () {
+    return {
+      documentId: '',
+      fields: {
+        uid: null,
+        title: null,
+        description: null,
+        ctaLink: null,
+        ctaText: null,
+        url: null,
+        banner_img: null,
+        article_content: null
+      }
+    }
+  },
+  methods: {
+    getContent (uid) {
+      this.$prismic.client.getSingle('main_banner')
+        .then((document) => {
+          if (document) {
+            this.documentId = document.id
+            this.fields.uid = document.uid
+            this.fields.title = document.data.title
+            this.fields.description = document.data.description
+            this.fields.ctaLink = document.data.cta_link
+            this.fields.ctaText = document.data.cta_text
+            this.fields.banner_img = document.data.banner_img
+            this.fields.url =  document.data.banner_img.url
+            this.fields.article_content =  document.data.article_content
+          } else {
+            this.$router.push({ name: 'not-found' })
+          }
+        })
+    }
+  },
+  created () {
+    this.getContent(this.$route.params.uid)
+  },
+  beforeRouteUpdate (to, from, next) {
+    this.getContent(to.params.uid)
+    next()
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import "./../scss/main";
+
+/* Main Section */
+/* Smartphones (portrait and landscape) ----------- */
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+  .main-content-wrapper {
+    grid-row: 2 / 8;
+    grid-column: 1 / -1;
+    /* Styling */
+    background-size: cover;
+    background-position: 50% 50%;
+    width: 100%;
+  }
+  .main-content-overlay {
+    background-color: rgba(0, 0, 0, 0.4);
+    background: linear-gradient(to top, rgba(0, 0, 0, 0.4), transparent 100%);
+  }
+  /* Main Section Headline H1 */
+  .main-content-h1-w {
+    text-align: center;
+  }
+  .main-content-h1 {
+    font-family: $font-bold;
+    color: white;
+    font-size: 3rem;
+    text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+    margin-top: 4.5em;
+  }
+  .main-content-h1:hover {
+    cursor: pointer;
+    text-decoration: underline;
+    text-decoration-color: $primary-color;
+  }
+
+}
+
+/* Desktops and laptops ----------- */
+@media only screen and (min-width: 1224px) {
+  /* Main Section background Image*/
+  .main-content-wrapper {
+    grid-row: 2 / 8;
+    grid-column: 1 / -1;
+    /* height: 480px; */
+    /* Styling */
+    background-size: cover;
+    background-position: 50% 50%;
+    height: 480px;
+  }
+  .main-content-overlay {
+    background-color: rgba(0, 0, 0, 0.4);
+    background: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.4),
+      transparent 100%
+    );
+  }
+  /* Main Section Headline H1 */
+  .main-content-h1-w {
+    width: 980px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .main-content-h1 {
+    font-family: $font-bold;
+    color: white;
+    font-size: 5.125rem;
+    text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  }
+  .main-content-h1:hover {
+    cursor: pointer;
+    text-decoration: underline;
+    text-decoration-color: $primary-color;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+        <navbar />
+        <main-banner />
+    </div>
+</template>
+
+<script>
+import Navbar from "./../components/NavBar/Navbar";
+import MainBanner from "./../components/MainBanner";
+
+export default {
+  name: "app",
+  components: {
+    Navbar,
+    MainBanner
+  }
+}
+</script>
+
+<style scoped>
+/* Smartphones (portrait and landscape) ----------- */
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+  /* Our  Main Container */
+  #app {
+    height: auto;
+    display: grid;
+  }
+}
+/* Desktops and laptops ----------- */
+@media only screen and (min-width: 1224px) {
+  #app {
+    height: auto;
+    display: grid;
+    grid-gap: 0;
+    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+    grid-auto-rows: 25%;
+    grid-auto-flow: dense;
+  }
+}
+</style>
